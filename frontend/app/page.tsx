33f959270184158d8d252d7c63e888811d2aafd2
Akhilandeshwari <akhilandeshwarichinthala@gmail.com>
2026-07-19 14:56:11 +0530

    Connect frontend with backend API

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,50 +1,66 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
+  const [orders, setOrders] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/orders")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch orders");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setOrders(data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setError("Failed to load data");
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <>
       <Navbar />
 
-<main
-  style={{
-    minHeight: "90vh",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-    padding: "20px",
-  }}
->
-  <h1 style={{ fontSize: "48px", marginBottom: "20px" }}>
-    MetricMind
-  </h1>
+      <main style={{ padding: "30px" }}>
+        <h1>MetricMind</h1>
 
-  <p
-    style={{
-      fontSize: "20px",
-      maxWidth: "700px",
-      marginBottom: "30px",
-    }}
-  >
-    AI-powered Semantic Business Intelligence Engine that helps users
-    ask business questions and receive accurate insights.
-  </p>
+        {loading && <p>Loading...</p>}
 
-  <button
-    style={{
-      backgroundColor: "#2563eb",
-      color: "white",
-      padding: "15px 30px",
-      border: "none",
-      borderRadius: "8px",
-      fontSize: "18px",
-      cursor: "pointer",
-    }}
-  >
-    Ask a Business Question
-  </button>
-</main>
+        {error && <p>{error}</p>}
+
+        {!loading && !error && (
+          <table border={1} cellPadding={10}>
+            <thead>
+              <tr>
+                <th>Order ID</th>
+                <th>Customer</th>
+                <th>Product</th>
+                <th>Sales</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {orders.slice(0, 10).map((order: any, index) => (
+                <tr key={index}>
+                  <td>{order["Order.ID"]}</td>
+                  <td>{order["Customer.Name"]}</td>
+                  <td>{order["Product.Name"]}</td>
+                  <td>{order["Sales"]}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </main>
     </>
   );
 }
